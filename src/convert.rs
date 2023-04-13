36e482b3256b35f137d59ba7aed2d682818b39c8
MSSRPRAD <pradyumna.malladi2003@gmail.com>
2023-04-14 01:34:29 +0530

    "small changes"

--- src/convert.rs
+++ src/convert.rs
@@ -3,12 +3,11 @@ use crate::functions::identify_type;
 
 pub fn convert_indic_to_roman(input: String, script: &Script) -> String {
     for i in 0..input.len(){
-        let mut c: char = 'x';
-        if let Some(val) = input.chars().nth(i) {
+        let c: char;
+        if let Some(_val) = input.chars().nth(i) {
             c = input.chars().nth(i).unwrap();
-            println!("\n\n{:?},{:?}",c,identify_type(c, script));
+            println!("{:?},{:?}",c,identify_type(c, script));
         }
     }
-    println!("\n\nDONE\n\n");
     "Some Error Occured!".to_string()
 }
--- src/functions.rs
+++ src/functions.rs
@@ -1,14 +1,14 @@
 use crate::read_mappings::Script;
 
-pub fn identify_type(c: char, data: &Script) -> String {
-    if data.consonants.main.contains(&c.to_string()) {return "consonants.main".to_string()}
-    else if data.vowels.main.contains(&c.to_string()) {return "vowels.main".to_string()}
-    else if data.vowelsigns.main.contains(&c.to_string()) {return "vowelsigns.main".to_string()}
-    else if data.vowelsigns.virama.contains(&c.to_string()) {return "vowelsigns.virama".to_string()}
-    else if data.numerals.contains(&c.to_string()) {return "numerals".to_string()}
-    else if data.others.symbols.contains(&c.to_string()) {return "data.others.symbols".to_string()}
-    else if data.others.aytham.contains(&c.to_string()) {return "data.others.aytham".to_string()}
-    else if c==' ' {return "space".to_string()}
-    else if c=='\n' {return "new-line".to_string()}
-    else {return String::from("Could not identify")}
+pub fn identify_type(c: char, data: &Script) -> (String, usize) {
+    if data.consonants.main.contains(&c.to_string().to_string()) {return ("consonants.main".to_string(), data.consonants.main.iter().position(|x| x.eq(&c.clone().to_string())).unwrap())}
+    else if data.vowels.main.contains(&c.to_string().to_string()) {return ("vowels.main".to_string(), data.vowels.main.iter().position(|x| x.eq(&c.clone().to_string())).unwrap())}
+    else if data.vowelsigns.main.contains(&c.to_string().to_string()) {return ("vowelsigns.main".to_string(), data.vowelsigns.main.iter().position(|x| x.eq(&c.clone().to_string())).unwrap())}
+    else if data.vowelsigns.virama.contains(&c.to_string().to_string()) {return ("vowelsigns.virama".to_string(), data.vowelsigns.virama.iter().position(|x| x.eq(&c.clone().to_string())).unwrap())}
+    else if data.numerals.contains(&c.to_string().to_string()) {return ("numerals".to_string(), data.numerals.iter().position(|x| x.eq(&c.clone().to_string())).unwrap())}
+    else if data.others.symbols.contains(&c.to_string().to_string()) {return ("data.others.symbols".to_string(), data.others.symbols.iter().position(|x| x.eq(&c.clone().to_string())).unwrap())}
+    else if data.others.aytham.contains(&c.to_string().to_string()) {return ("data.others.aytham".to_string(), data.others.aytham.iter().position(|x| x.eq(&c.clone().to_string())).unwrap())}
+    else if c==' ' {return ("space".to_string(), 999)}
+    else if c=='\n' {return ("new-line".to_string(), 999)}
+    else {return (String::from("Could not identify"), 999)}
 }
