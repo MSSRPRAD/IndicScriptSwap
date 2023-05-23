210d324aa56bf68eaa2ed62823728c82bbc4a1e5
Malladi Pradyumna <f20210367@hyderabad.bits-pilani.ac.in>
2023-05-23 21:55:50 +0530

    Added test for convert_indic_to_roman

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use transliterate_ferris::convert::convert_indic_to_indic;
+    use transliterate_ferris::convert::{convert_indic_to_indic, convert_indic_to_roman};
     use transliterate_ferris::data::HASH_MAP;
     #[test]
     fn test_indic_to_indic() {
@@ -28,6 +28,34 @@ mod tests {
         let telugu = foo.get("telugu").unwrap();
         let devanagari = foo.get("devanagari").unwrap();
         let converted = convert_indic_to_indic(input, telugu, devanagari);
+        assert_eq!(expected, converted);
+    }
+    #[test]
+    fn test_indic_to_roman_1() {
+        let foo = &HASH_MAP;
+        let input = "తపఃస్వాధ్యాయనిరతం తపస్వీ వాగ్విదాం వరమ్ ।
+                నారదం పరిపప్రచ్ఛ వాల్మీకిర్మునిపుంగవమ్ ॥ ౧ ॥
+                కో న్వస్మిన్సామ్ప్రతం లోకే గుణవాన్కశ్చ వీర్యవాన్ ।
+                ధర్మజ్ఞశ్చ కృతజ్ఞశ్చ సత్యవాక్యో దృఢవ్రతః ॥ ౨ ॥
+                చారిత్రేణ చ కో యుక్తః సర్వభూతేషు కో హితః ।
+                విద్వాన్కః కః సమర్థశ్చ కశ్చైకప్రియదర్శనః ॥ ౩ ॥
+                ఆత్మవాన్కో జితక్రోధో ద్యుతిమాన్కోఽనసూయకః ।
+                కస్య బిభ్యతి దేవాశ్చ జాతరోషస్య సంయుగే ॥ ౪ ॥
+                ఏతదిచ్ఛామ్యహం శ్రోతుం పరం కౌతూహలం హి మే ।
+                మహర్షే త్వం సమర్థోఽసి జ్ఞాతుమేవంవిధం నరమ్ ॥ ౫ ॥";
+        let expected = "tapaHsvADyAyanirataM tapasvI vAgvidAM varam .
+                nAradaM paripapracCa vAlmIkirmunipuMgavam .. 1 ..
+                ko nvasminsAmprataM loke guRavAnkaSca vIryavAn .
+                DarmajYaSca kftajYaSca satyavAkyo dfQavrataH .. 2 ..
+                cAritreRa ca ko yuktaH sarvaBUtezu ko hitaH .
+                vidvAnkaH kaH samarTaSca kaScEkapriyadarSanaH .. 3 ..
+                AtmavAnko jitakroDo dyutimAnko'nasUyakaH .
+                kasya biByati devASca jAtarozasya saMyuge .. 4 ..
+                etadicCAmyahaM SrotuM paraM kOtUhalaM hi me .
+                maharze tvaM samarTo'si jYAtumevaMviDaM naram .. 5 ..";
+        let telugu = foo.get("telugu").unwrap();
+        let slp1 = foo.get("slp1").unwrap();
+        let converted = convert_indic_to_roman(input, telugu, slp1);
         assert_eq!(expected, converted);
     }
 }
