9345ab219dd23d9a5a15bb1306f9a2efd30897bf
Malladi Pradyumna <f20210367@hyderabad.bits-pilani.ac.in>
2023-05-24 01:04:23 +0530

    Trivial rearrangement of code blocks

--- src/convert.rs
+++ src/convert.rs
@@ -172,111 +172,6 @@ pub fn convert_indic_to_roman(input: &str, source: &Script, destination: &Script
     output.replace("×", "")
 }
 
-pub fn convert_roman_to_roman(input: &str, source: &Script, destination: &Script) -> String {
-    // Make a hashmap from source characters to corresponding destination ones
-    // Since all we need now is the consonants, numerals, vowels, vowelsigns, others, we will make only for them for now.
-    let hash_map_consonants_main = make_hash_map(source, destination, 1);
-    let hash_map_vowels_main = make_hash_map(source, destination, 2);
-    let hash_map_vowelsigns_main = make_hash_map(source, destination, 3);
-    let hash_map_vowelsigns_virama = make_hash_map(source, destination, 4);
-    let hash_map_numerals = make_hash_map(source, destination, 5);
-    let hash_map_others_aytham = make_hash_map(source, destination, 6);
-    let hash_map_combiningsigns_ayogavaha = make_hash_map(source, destination, 7);
-    let hash_map_others_symbols = make_hash_map(source, destination, 8);
-    // The logic for transliteration is very simple. First we check if the next two characters have a mapping.
-    // If they have we push the corresponding destination string to the output.
-    // If they don't we check the mapping for the current character only and push the corresponding destination string to the output.
-    let mut output: String = String::new();
-    let mut skip = false;
-    let len = input.chars().count();
-    let chars: Vec<char> = input.chars().collect();
-    for i in 0..len {
-        if skip {
-            skip = false;
-            continue;
-        }
-        let s = &mut String::new();
-        let foo = &mut String::new();
-        {
-            let t: CharType;
-
-            if i < len - 1 {
-                foo.push_str(&chars[i].to_string());
-                foo.push_str(&chars[i + 1].to_string());
-                if identify_type(
-                    foo,
-                    &hash_map_consonants_main,
-                    &hash_map_vowels_main,
-                    &hash_map_vowelsigns_main,
-                    &hash_map_vowelsigns_virama,
-                    &hash_map_numerals,
-                    &hash_map_others_aytham,
-                    &hash_map_combiningsigns_ayogavaha,
-                    &hash_map_others_symbols,
-                ) == CharType::CouldNotIdentify
-                {
-                    // println!("false| {:?}", foo);
-                    s.push_str(&chars[i].to_string());
-                } else {
-                    // println!("true| {:?} | ", foo);
-                    s.push_str(&chars[i].to_string());
-                    s.push_str(&chars[i + 1].to_string());
-                    skip = true;
-                }
-            } else {
-                s.push_str(&chars[i].to_string());
-            }
-            t = identify_type(
-                s,
-                &hash_map_consonants_main,
-                &hash_map_vowels_main,
-                &hash_map_vowelsigns_main,
-                &hash_map_vowelsigns_virama,
-                &hash_map_numerals,
-                &hash_map_others_aytham,
-                &hash_map_combiningsigns_ayogavaha,
-                &hash_map_others_symbols,
-            );
-            // if skip {
-            //     println!("{:?}", t);
-            // }
-            match t {
-                CharType::ConsonantsMain => {
-                    output.push_str(hash_map_consonants_main.get(s.as_str()).unwrap());
-                }
-                CharType::VowelsMain => {
-                    output.push_str(hash_map_vowels_main.get(s.as_str()).unwrap());
-                }
-                CharType::VowelSignsMain => {
-                    output.push_str(hash_map_vowelsigns_main.get(s.as_str()).unwrap());
-                }
-                CharType::VowelSignsVirama => {
-                    output.push_str(hash_map_vowelsigns_virama.get(s.as_str()).unwrap());
-                }
-                CharType::OthersSymbols => {
-                    output.push_str(hash_map_others_symbols.get(s.as_str()).unwrap());
-                }
-                CharType::OthersAytham => {
-                    output.push_str(hash_map_others_aytham.get(s.as_str()).unwrap());
-                }
-                CharType::CombiningSignsAyogavaha => {
-                    output.push_str(hash_map_combiningsigns_ayogavaha.get(s.as_str()).unwrap());
-                }
-                CharType::Space => {
-                    output.push_str(" ");
-                }
-                CharType::NewLine => {
-                    output.push_str("\n");
-                }
-                CharType::Numerals => {
-                    output.push_str(hash_map_numerals.get(s.as_str()).unwrap());
-                }
-                _ => {}
-            };
-        }
-    }
-    output
-}
 
 pub fn convert_roman_to_indic(input: &str, source: &Script, destination: &Script) -> String {
     // Make a hashmap from source characters to corresponding destination ones
@@ -518,5 +413,112 @@ pub fn convert_roman_to_indic(input: &str, source: &Script, destination: &Script
         };
     }
 
+    output
+}
+
+
+pub fn convert_roman_to_roman(input: &str, source: &Script, destination: &Script) -> String {
+    // Make a hashmap from source characters to corresponding destination ones
+    // Since all we need now is the consonants, numerals, vowels, vowelsigns, others, we will make only for them for now.
+    let hash_map_consonants_main = make_hash_map(source, destination, 1);
+    let hash_map_vowels_main = make_hash_map(source, destination, 2);
+    let hash_map_vowelsigns_main = make_hash_map(source, destination, 3);
+    let hash_map_vowelsigns_virama = make_hash_map(source, destination, 4);
+    let hash_map_numerals = make_hash_map(source, destination, 5);
+    let hash_map_others_aytham = make_hash_map(source, destination, 6);
+    let hash_map_combiningsigns_ayogavaha = make_hash_map(source, destination, 7);
+    let hash_map_others_symbols = make_hash_map(source, destination, 8);
+    // The logic for transliteration is very simple. First we check if the next two characters have a mapping.
+    // If they have we push the corresponding destination string to the output.
+    // If they don't we check the mapping for the current character only and push the corresponding destination string to the output.
+    let mut output: String = String::new();
+    let mut skip = false;
+    let len = input.chars().count();
+    let chars: Vec<char> = input.chars().collect();
+    for i in 0..len {
+        if skip {
+            skip = false;
+            continue;
+        }
+        let s = &mut String::new();
+        let foo = &mut String::new();
+        {
+            let t: CharType;
+
+            if i < len - 1 {
+                foo.push_str(&chars[i].to_string());
+                foo.push_str(&chars[i + 1].to_string());
+                if identify_type(
+                    foo,
+                    &hash_map_consonants_main,
+                    &hash_map_vowels_main,
+                    &hash_map_vowelsigns_main,
+                    &hash_map_vowelsigns_virama,
+                    &hash_map_numerals,
+                    &hash_map_others_aytham,
+                    &hash_map_combiningsigns_ayogavaha,
+                    &hash_map_others_symbols,
+                ) == CharType::CouldNotIdentify
+                {
+                    // println!("false| {:?}", foo);
+                    s.push_str(&chars[i].to_string());
+                } else {
+                    // println!("true| {:?} | ", foo);
+                    s.push_str(&chars[i].to_string());
+                    s.push_str(&chars[i + 1].to_string());
+                    skip = true;
+                }
+            } else {
+                s.push_str(&chars[i].to_string());
+            }
+            t = identify_type(
+                s,
+                &hash_map_consonants_main,
+                &hash_map_vowels_main,
+                &hash_map_vowelsigns_main,
+                &hash_map_vowelsigns_virama,
+                &hash_map_numerals,
+                &hash_map_others_aytham,
+                &hash_map_combiningsigns_ayogavaha,
+                &hash_map_others_symbols,
+            );
+            // if skip {
+            //     println!("{:?}", t);
+            // }
+            match t {
+                CharType::ConsonantsMain => {
+                    output.push_str(hash_map_consonants_main.get(s.as_str()).unwrap());
+                }
+                CharType::VowelsMain => {
+                    output.push_str(hash_map_vowels_main.get(s.as_str()).unwrap());
+                }
+                CharType::VowelSignsMain => {
+                    output.push_str(hash_map_vowelsigns_main.get(s.as_str()).unwrap());
+                }
+                CharType::VowelSignsVirama => {
+                    output.push_str(hash_map_vowelsigns_virama.get(s.as_str()).unwrap());
+                }
+                CharType::OthersSymbols => {
+                    output.push_str(hash_map_others_symbols.get(s.as_str()).unwrap());
+                }
+                CharType::OthersAytham => {
+                    output.push_str(hash_map_others_aytham.get(s.as_str()).unwrap());
+                }
+                CharType::CombiningSignsAyogavaha => {
+                    output.push_str(hash_map_combiningsigns_ayogavaha.get(s.as_str()).unwrap());
+                }
+                CharType::Space => {
+                    output.push_str(" ");
+                }
+                CharType::NewLine => {
+                    output.push_str("\n");
+                }
+                CharType::Numerals => {
+                    output.push_str(hash_map_numerals.get(s.as_str()).unwrap());
+                }
+                _ => {}
+            };
+        }
+    }
     output
 }
